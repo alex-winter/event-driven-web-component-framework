fc4e54dc84454ddbb737c0466c3df8db23685d0a
Alex Winter <ajwinter42@gmail.com>
2025-07-08 22:27:16 +0100

    fix for global style getting

--- src/GlobalStyleRegistry.ts
+++ src/GlobalStyleRegistry.ts
@@ -1,6 +1,8 @@
 const globalSheets = new Map<string, CSSStyleSheet>()
 
-export function getGlobalStyleSheet(css: string): CSSStyleSheet {
+export async function getGlobalStyleSheet(file: string): Promise<CSSStyleSheet> {
+    const response = await fetch(file)
+    const css = await response.text()
     if (!globalSheets.has(css)) {
         const sheet = new CSSStyleSheet()
         sheet.replaceSync(css)
