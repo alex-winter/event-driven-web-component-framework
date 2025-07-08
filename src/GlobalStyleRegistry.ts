9aab480005e8c196678e9f3f702c52f9122f37e6
Alex Winter <ajwinter42@gmail.com>
2025-07-08 22:33:21 +0100

    cache key should be file

--- src/GlobalStyleRegistry.ts
+++ src/GlobalStyleRegistry.ts
@@ -3,11 +3,11 @@ const globalSheets = new Map<string, CSSStyleSheet>()
 export async function getGlobalStyleSheet(file: string): Promise<CSSStyleSheet> {
     const response = await fetch(file)
     const css = await response.text()
-    if (!globalSheets.has(css)) {
+    if (!globalSheets.has(file)) {
         const sheet = new CSSStyleSheet()
         sheet.replaceSync(css)
-        globalSheets.set(css, sheet)
+        globalSheets.set(file, sheet)
     }
 
-    return globalSheets.get(css)!
+    return globalSheets.get(file)!
 }
